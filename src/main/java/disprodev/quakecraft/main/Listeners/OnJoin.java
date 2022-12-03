00adbfc2de62de24f77dd296802b20e036385e8e
DisProPC <disprodev@gmail.com>
2022-12-03 18:00:34 +0100

    Maven change code + onjoin

--- src/main/java/disprodev/quakecraft/main/Listeners/OnJoin.java
+++ src/main/java/disprodev/quakecraft/main/Listeners/OnJoin.java
@@ -1,6 +1,10 @@
 package disprodev.quakecraft.main.Listeners;
 
+import disprodev.quakecraft.main.GStats;
 import disprodev.quakecraft.main.Main;
+import disprodev.quakecraft.main.ScordBoard.FastBoard;
+import org.bukkit.GameMode;
+import org.bukkit.Location;
 import org.bukkit.entity.Player;
 import org.bukkit.event.Listener;
 import org.bukkit.event.player.PlayerJoinEvent;
@@ -9,12 +13,32 @@ public class OnJoin implements Listener {
     private Main main;
 
 
-    public OnJoin(PlayerJoinEvent e){
+    public OnJoin(PlayerJoinEvent e) {
         Player p = e.getPlayer();
-        main.getPlayers().add(p);
-        if()
-
-
+        if(main.getPlayers().size() == 8 || main.getStat() != GStats.WAITING){
+            p.setGameMode(GameMode.SPECTATOR);
+            Location loc = new Location(p.getWorld(),50 ,139 ,42 ,4F ,9F );
+            p.teleport(loc);
+        }
+        if (!main.getPlayers().contains(p)){
+            main.getPlayers().add(p);
     }
+    }
+    private void updateBoard(FastBoard board) {
+        board.updateLines(
+                "",
+                "§6§lProfil",
+                "   §6Pseudo §f» §f" + board.getPlayer().getName(),
+                "   §6Stats §f» §cAttente",
+                "",
+                "§6§lServeur",
+                "   §6Connectés §f» §f" + main.getPlayers().size(),
+                "",
+                main.prf
+        );
+
+
+}
+
 
 }
--- src/main/java/disprodev/quakecraft/main/Main.java
+++ src/main/java/disprodev/quakecraft/main/Main.java
@@ -1,30 +1,51 @@
 package disprodev.quakecraft.main;
 
+import disprodev.quakecraft.main.ScordBoard.FastBoard;
 import net.md_5.bungee.api.ChatColor;
 import org.bukkit.Bukkit;
 import org.bukkit.entity.Player;
+import org.bukkit.event.EventHandler;
+import org.bukkit.event.Listener;
+import org.bukkit.event.player.PlayerJoinEvent;
+import org.bukkit.event.player.PlayerQuitEvent;
 import org.bukkit.plugin.java.JavaPlugin;
 
-import java.util.ArrayList;
-import java.util.List;
+import java.util.*;
 
-public final class Main extends JavaPlugin {
+public final class Main extends JavaPlugin implements Listener {
+    public void setStat(GStats stat) {
+        this.stat = stat;
+    }
+
+    public GStats getStat() {
+        return stat;
+    }
+
     public List<Player> getPlayers() {
         return players;
     }
+    private GStats stat;
 
     public void setPlayers(List<Player> players) {
         this.players = players;
     }
-
+    private final Map<UUID, FastBoard> boards = new HashMap<>();
     List<Player>players = new ArrayList<>();
     private Main instance;
-    public String prf= "§eQuake";
+    public String prf= "§6Quake";
     @Override
     public void onEnable() {
         instance = this;
         msgconsole("§aOn",prf);
+        getServer().getPluginManager().registerEvents(this, this);
+
+        getServer().getScheduler().runTaskTimer(this, () -> {
+            for (FastBoard board : this.boards.values()) {
+                updateBoard(board);
+            }
+        }, 0, 20);
     }
+
     private void msgconsole(String s,String n) {
         Bukkit.getConsoleSender().sendMessage(ChatColor.translateAlternateColorCodes('&', "&5=========="+ n+"§5=========="));
         Bukkit.getConsoleSender().sendMessage(ChatColor.translateAlternateColorCodes('&', "&eCeci est le plugin de DisPro"));
@@ -36,4 +57,40 @@ public final class Main extends JavaPlugin {
     public void onDisable() {
         msgconsole("§cOFF",prf);
     }
+    @EventHandler
+    public void onJoin(PlayerJoinEvent e) {
+        Player player = e.getPlayer();
+
+        FastBoard board = new FastBoard(player);
+
+        board.updateTitle(prf);
+
+        this.boards.put(player.getUniqueId(), board);
+    }
+
+    @EventHandler
+    public void onQuit(PlayerQuitEvent e) {
+        Player player = e.getPlayer();
+
+        FastBoard board = this.boards.remove(player.getUniqueId());
+
+        if (board != null) {
+            board.delete();
+        }
+    }
+
+    private void updateBoard(FastBoard board) {
+        board.updateLines(
+                "",
+                "§6§lProfil",
+                "   §6Pseudo §f» §f" + board.getPlayer().getName(),
+                "   §6Stats §f» Loadings...",
+                "",
+                "§6§lServeur",
+                "   §6Connectés §f» §f" + getPlayers().size(),
+                "",
+                prf
+        );
+    }
+
 }
