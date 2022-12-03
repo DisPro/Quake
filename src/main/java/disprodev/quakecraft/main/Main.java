7266a0e946133124fa52773ef6148ce8c9ae92c7
DisProPC <disprodev@gmail.com>
2022-12-03 17:18:51 +0100

    Initial commit

---
+++ src/main/java/disprodev/quakecraft/main/Main.java
@@ -0,0 +1,26 @@
+package disprodev.quakecraft.main;
+
+import net.md_5.bungee.api.ChatColor;
+import org.bukkit.Bukkit;
+import org.bukkit.plugin.java.JavaPlugin;
+
+public final class Main extends JavaPlugin {
+    private Main instance;
+    public String prf= "§eQuake";
+    @Override
+    public void onEnable() {
+        instance = this;
+        msgconsole("§aOn",prf);
+    }
+    private void msgconsole(String s,String n) {
+        Bukkit.getConsoleSender().sendMessage(ChatColor.translateAlternateColorCodes('&', "&5=========="+ n+"§5=========="));
+        Bukkit.getConsoleSender().sendMessage(ChatColor.translateAlternateColorCodes('&', "&eCeci est le plugin de DisPro"));
+        Bukkit.getConsoleSender().sendMessage(ChatColor.translateAlternateColorCodes('&', "§e"));
+        Bukkit.getConsoleSender().sendMessage(ChatColor.translateAlternateColorCodes('&', "§eEtat du plugin :" + s));
+        Bukkit.getConsoleSender().sendMessage(ChatColor.translateAlternateColorCodes('&', "&5=========="+ n+"§5=========="));
+    }
+    @Override
+    public void onDisable() {
+        msgconsole("§cOFF",prf);
+    }
+}
